ba3f5fccbb921d17bdedbe6bafcfd26197fc2177
ChelseaTolnai <ChelseaTolnai@gmail.com>
2019-01-18 12:47:22 -0500

    Inabled Card filtering upon tab selection.

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -2,17 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Tab = props => {
-  /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
-      if they match, the className should be: 'tab active-tab', 
-      if it is not it should just be 'tab'*/
+
+  const findClassName = props => 
+    props.tab === props.selectedTab 
+    ? 'tab active-tab'
+    : 'tab'
+  ;
+  
   return (
     <div
-      className={''}
-      // onClick={() => {
-      //   /* Replace this dummy click handler function with your selectTabHandler function from props 
-      //    you'll need to pass the `tab` in as an argument to this handler. */
-      // }}
-      onClick={props.selectedTabHandler}
+      className={findClassName(props)}
+      onClick={ () => props.selectedTabHandler(props.tab)}
     >
       {props.tab.toUpperCase()}
     </div>
